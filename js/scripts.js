7423ec157912000e4536e449761a2dd8616641a6
M. Hille <marciausgoslar@gmail.com>
2019-07-21 14:09:56 +0200

    Added dom list and pokemon class

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-(function() {})();
+(function() {
+  
+    // DOM Elements
+  var $pokemonList = $(".main-content_pokemon-list");
+
+  // Container for storing pokemon relevant data
+  function Pokemon(name, detailsUrl) {
+    this.name = name;
+    this.details = {};
+    this.detailsUrl = detailsUrl;
+  }
+})();
